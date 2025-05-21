9934b2b847b1d161a7c1b80faebb926e1460497b
DenisHerzhyk <denis.herzhyk88@gmail.com>
2025-05-21 23:18:56 +0200

    Added types folder for image types

---
+++ src/types/images.d.ts
@@ -0,0 +1,19 @@
+declare module '*.jpg' {
+    const content: string;
+    export default content;
+}
+
+declare module '*.webp' {
+    const content: string;
+    export default content;
+}
+
+declare module '*.svg' {
+    const content: string;
+    export default content;
+}
+
+declare module '*.mp4' {
+    const content: string;
+    export default content;
+} 
